05ff0db0edcfbbba1b5336e076f983848b616870
gabrieltassinari <kievhotkatwo@gmail.com>
2022-04-03 15:32:36 -0300

    modules folders

---
+++ modules-privacy/target/debug/deps/privacy-161c7b0da0a3aef6.d
@@ -0,0 +1,7 @@
+/home/gear/rust-book/modules-privacy/target/debug/deps/privacy-161c7b0da0a3aef6.rmeta: src/lib.rs
+
+/home/gear/rust-book/modules-privacy/target/debug/deps/libprivacy-161c7b0da0a3aef6.rlib: src/lib.rs
+
+/home/gear/rust-book/modules-privacy/target/debug/deps/privacy-161c7b0da0a3aef6.d: src/lib.rs
+
+src/lib.rs:
---
+++ modules-privacy/target/debug/libprivacy.d
@@ -0,0 +1,1 @@
+/home/gear/rust-book/modules-privacy/target/debug/libprivacy.rlib: /home/gear/rust-book/modules-privacy/src/lib.rs
