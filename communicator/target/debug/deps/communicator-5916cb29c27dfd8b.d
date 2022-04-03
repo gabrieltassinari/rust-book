ae48155ccd022609f0e54aeefea5fe03cc5824f6
gabrieltassinari <kievhotkatwo@gmail.com>
2022-04-03 15:32:09 -0300

    more communicator changes

---
+++ communicator/target/debug/deps/communicator-5916cb29c27dfd8b.d
@@ -0,0 +1,8 @@
+/home/gear/rust-book/communicator/target/debug/deps/communicator-5916cb29c27dfd8b: src/lib.rs src/client.rs src/network/mod.rs src/network/server.rs
+
+/home/gear/rust-book/communicator/target/debug/deps/communicator-5916cb29c27dfd8b.d: src/lib.rs src/client.rs src/network/mod.rs src/network/server.rs
+
+src/lib.rs:
+src/client.rs:
+src/network/mod.rs:
+src/network/server.rs:
---
+++ communicator/target/debug/deps/communicator-eb37861fb3e65642.d
@@ -0,0 +1,5 @@
+/home/gear/rust-book/communicator/target/debug/deps/communicator-eb37861fb3e65642: src/main.rs
+
+/home/gear/rust-book/communicator/target/debug/deps/communicator-eb37861fb3e65642.d: src/main.rs
+
+src/main.rs:
