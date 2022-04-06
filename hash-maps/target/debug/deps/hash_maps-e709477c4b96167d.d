8a053f2a1ee5e7bce32691a015693dbd8e500fae
gabrieltassinari <kievhotkatwo@gmail.com>
2022-04-06 14:42:08 -0300

    hashmaps folder

---
+++ hash-maps/target/debug/deps/hash_maps-e709477c4b96167d.d
@@ -0,0 +1,5 @@
+/home/gear/rust-book/hash-maps/target/debug/deps/hash_maps-e709477c4b96167d: src/main.rs
+
+/home/gear/rust-book/hash-maps/target/debug/deps/hash_maps-e709477c4b96167d.d: src/main.rs
+
+src/main.rs:
---
+++ hash-maps/target/debug/hash-maps.d
@@ -0,0 +1,1 @@
+/home/gear/rust-book/hash-maps/target/debug/hash-maps: /home/gear/rust-book/hash-maps/src/main.rs
